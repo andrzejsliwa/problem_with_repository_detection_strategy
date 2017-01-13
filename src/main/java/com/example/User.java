d342e81c9b816f64d9b8159c699d23ec503522e8
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2017-01-13 16:57:33 +0100

    initial import

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+}
